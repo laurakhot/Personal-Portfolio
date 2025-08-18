483e3d71daf3a5d9289a2be7929affadd6325b1d
laurakhot <laurak11@uw.edu>
2025-08-17 23:23:03 -0700

    Added header with animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"]});
 
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}> 
         <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div> 
         <div className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div> 
+        <Header />
         {children} 
         </body>
     </html>
