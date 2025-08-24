c415a02c0feeb66ce7b796b58ff5f46a797185c4
laurakhot <laurak11@uw.edu>
2025-08-24 00:16:57 -0700

    Continued project section

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
+import Image from 'next/image';
 
 export default function Projects() {
   return (
@@ -22,6 +23,15 @@ type ProjectCardProps = (typeof projectsData)[number];
 
 function ProjectCard({title, description, tags, imageUrl } : ProjectCardProps) {
   return (
-    <div>{title}</div>
-  )
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8">
+      <h1 className="text-2xl font-semibold">{title}</h1>
+      <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+            <li className="bg-black px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
+        ))}
+      </ul>
+      <Image src={imageUrl} alt={description} quality={95}/>
+    </section>
+  );
 }
