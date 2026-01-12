f35c5a94edfd4b8f0da76e842ac1b173c0b74bb6
laurakhot <laurak11@uw.edu>
2026-01-12 00:15:16 -0800

    Updated Resume

--- lib/data.ts
+++ lib/data.ts
@@ -33,10 +33,6 @@ export const links = [
     name: "Skills",
     hash: "#skills",
   },
-  // {
-  //   name: "Education",
-  //   hash: "#education",
-  // },
   {
     name: "Projects",
     hash: "#projects",
